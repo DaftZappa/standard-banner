b110802c6a1bdcc59487a3f2d234f9e6eb7a023c
Niels Wijers <nielswijers@gmail.com>
2013-06-25 16:08:34 +0200

    added animation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-$(function(){
-	$('.banner').click(function(){
-		window.open('http://www.google.nl','_blank')
-	})
+$(function() {
+	var w = $('.banner').width();
+	var h = $('.banner').height();
+	new TimelineMax()
+		.from('.banner', 0.2, {opacity: 0 })
+		.from('.content-container', 0.5, {opacity: 0 }, 0.5)
+		.from('.logo', 0.5, {opacity: 0}, 0.5)
+		.from('.p2u', 0.5, {opacity: 0}, 0.5)
+		.from('.phone', 0.5, {left: '+=' + w, top: '+=' + w }, 0.5)
+		.from('.sticker', 0.5, {left: '+=' + w, top: '+=' + w }, 0.7)
+		.to('.p2u', 0.5, {opacity: 0}, 4)
+		.from('.vodafone', 0.5, {opacity: 0}, 4.5);
+
+	$('.banner').click(function() {
+		window.open('http://www.google.nl', '_blank');
+	});
 });
