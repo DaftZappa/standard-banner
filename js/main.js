c410a7dd15acfaa13a341c5fb6e50aa28b685e8b
Siebe Hiemstra <siepprivate@gmail.com>
2013-06-28 12:27:13 +0200

    IE7 fixes and animations

--- js/main.js
+++ js/main.js
@@ -12,10 +12,18 @@ $(function() {
 			.from('.logo', 0.5, {opacity: (Modernizr.opacity ? 0 : 1)}, 0.5)
 			.from('.p2u', 0.5, {opacity: 0}, 0.5)
 			.from('.phone', 0.5, {left: '+=' + w, top: '+=' + w }, 0.5)
-			.from('.sticker', 0.5, {left: '+=' + w, top: '+=' + w }, 0.7)
+			.from('.sticker', 0.5, {left: '+=' + w, top: '+=' + w }, 0.7)			
 			.to('.p2u', 0.5, {opacity: 0}, 4)
-			.from('.vodafone', 0.5, {opacity: 0}, 4.5);
+			.from('.vodafone', 0.5, {opacity: 0}, 4.5)
+			.append(function (){ 
+				return new Pulse('.button',7);
+			},2) ;
 	}
 });
 
 
+var Pulse = function(target, repeat) {
+	return new TimelineMax({repeat:repeat})
+				.to(target, 0.2,{scale:1.1,alpha:1, ease:Circ.easeOut},"+=0")
+				.to(target, 0.2,{scale:1,alpha:1, ease:Circ.easeIn},"+=0");
+}
